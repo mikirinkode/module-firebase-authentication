edf23b750c891cbf4c4e61193d2c9cbbbc0bc93a
Muhammad Wafa <muhammadwafa105@gmail.com>
2023-08-08 13:54:49 +0700

    finish creating ui

---
+++ app/src/main/java/com/mikirinkode/module/firebaseauth/feature/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.mikirinkode.module.firebaseauth.feature.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mikirinkode.module.firebaseauth.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
